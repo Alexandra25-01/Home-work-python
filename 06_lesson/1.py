ce1926be62986ec44f8e6eb926728e27b55d1937
Alexandra <andronova-alexandra25@yandex.ru>
2025-10-15 22:20:03 +0500

    исправления6

--- 06_lesson/1.py
+++ 06_lesson/1.py
@@ -1,20 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 service = Service(
     "C:/Users/PK-1/Downloads/geckodriver-v0.36.0-win64/geckodriver.exe"
     )
 
 driver = webdriver.Firefox(service=service)
-
-driver.implicitly_wait(16)
 driver.get("http://uitestingplayground.com/ajax")
 
 button = driver.find_element(By.CSS_SELECTOR, "button.btn")
 button.click()
 
-driver.implicitly_wait(19)
+waiter = WebDriverWait(driver, 20)
+green_text_element = waiter.until(
+    EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "p.bg-success"))
+)
 
 green_text = driver.find_element(By.CSS_SELECTOR, "p.bg-success").text
 print(green_text)
--- 06_lesson/3.py
+++ 06_lesson/3.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 service = Service(
     "C:/Users/PK-1/Downloads/chromedriver-win64 (1)/chromedriver-win64"
@@ -8,10 +10,25 @@ service = Service(
     )
 driver = webdriver.Chrome(service=service)
 
-driver.implicitly_wait(10)
 driver.get(
     "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
     )
+
+waiter = WebDriverWait(driver, 10)
+image1 = waiter.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "img#compass"))
+)
+image2 = waiter.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "img#calendar"))
+)
+
+image3 = waiter.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "img#award"))
+)
+
+image4 = waiter.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "img#landscape"))
+)
 
 src = driver.find_element(By.CSS_SELECTOR, 'img#award').get_attribute("src")
 print(src)
